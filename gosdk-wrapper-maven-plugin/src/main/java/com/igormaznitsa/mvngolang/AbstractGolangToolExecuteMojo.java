deff40ddcaaeb69da9bddb40fff9f3bacb022b29
Sebastian Rühl <sruehl@apache.org>
2025-03-12 14:07:03 +0100

    fix: unblock process because when input is not read
    Rationale: on some occasions a process might just block if the STDOUT is not read. In that case as with the non Thread variant we open the error stream and we don't get data, hence block forever. With this change it is ensure that both streams are getting read from.

--- gosdk-wrapper-maven-plugin/src/main/java/com/igormaznitsa/mvngolang/AbstractGolangToolExecuteMojo.java
+++ gosdk-wrapper-maven-plugin/src/main/java/com/igormaznitsa/mvngolang/AbstractGolangToolExecuteMojo.java
@@ -300,16 +300,18 @@ public abstract class AbstractGolangToolExecuteMojo extends AbstractGolangSdkAwa
 
   private void catchStream(final InputStream inputStream,
                            final Consumer<String> lineConsumer) {
-    try (final BufferedReader reader = new BufferedReader(
-        new InputStreamReader(inputStream,
-            Charset.defaultCharset()))) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        lineConsumer.accept(line);
+    new Thread(() -> { // It is important to do this async as on some occasions processes might block until the input is read
+      try (final BufferedReader reader = new BufferedReader(
+              new InputStreamReader(inputStream,
+                      Charset.defaultCharset()))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          lineConsumer.accept(line);
+        }
+      } catch (IOException ex) {
+        this.logError("IOException during input stream read: " + ex.getMessage());
       }
-    } catch (IOException ex) {
-      this.logError("IOException during input stream read: " + ex.getMessage());
-    }
+    }).start();
   }
 
   @Nullable
